4526e2462464dd47bf346c0ee2a52adb874b4a7b
milan <milon06@gmail.com>
2015-09-07 03:01:24 +0600

    Initial commit

---
+++ AutoLayoutTest/AutoLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutTest
+//
+//  Created by Milan on 9/7/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
